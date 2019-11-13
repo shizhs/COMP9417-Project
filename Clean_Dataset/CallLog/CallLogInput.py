e071bc15c8cc454f8a1ee35161b8bb3fc39d5bc9
Yiheng (NUC) <development.henryquan@gmail.com>
2019-11-13 22:22:20 +1100

    copy and paste, call_log is also done

--- Clean_Dataset/CallLog/CallLogInput.py
+++ Clean_Dataset/CallLog/CallLogInput.py
@@ -1,7 +1,32 @@
+# handle import here
 import sys
 sys.path.append('../../')
 
-from Clean_Dataset.Utils.csv_util import csv_util
+'''
+SmsInput
+'''
 
-sms = csv_util('../../StudentLife_Dataset/Inputs/call_log/', True)
+from Clean_Dataset.Utils.csv_util import csv_util
+import pandas as pd
+import numpy as np
+
+def percentage(data):
+    '''data is a DataFrame'''
+
+    array = np.array(data)
+    call_count = 0
+    for row in array:
+        for value in row[6:]:
+            if not str(value) == "nan":
+                call_count += 1
+                break
+
+    length = len(array)
+    if length == 0:
+        return 0.0
+    return call_count / length
+
+sms = csv_util('../../StudentLife_Dataset/Inputs/call_log/')
 sms.readAll()
+sms.process(percentage)
+sms.writeToCsv(['uid', 'percentage'], 'call_log.csv')
