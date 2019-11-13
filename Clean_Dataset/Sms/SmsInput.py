2dcb999460e02bb184e370376b84be3a910d1d8d
Yiheng (NUC) <development.henryquan@gmail.com>
2019-11-13 22:17:40 +1100

    sms.csv added (shockingly low...)

--- Clean_Dataset/Sms/SmsInput.py
+++ Clean_Dataset/Sms/SmsInput.py
@@ -1,13 +1,32 @@
+# handle import here
 import sys
 sys.path.append('../../')
 
+'''
+SmsInput
+'''
+
 from Clean_Dataset.Utils.csv_util import csv_util
+import pandas as pd
+import numpy as np
 
 def percentage(data):
     '''data is a DataFrame'''
-    return 0.1
 
-sms = csv_util('../../StudentLife_Dataset/Inputs/sms/', True)
+    array = np.array(data)
+    call_count = 0
+    for row in array:
+        for value in row[3:]:
+            if not str(value) == "nan":
+                call_count += 1
+                break
+
+    length = len(array)
+    if length == 0:
+        return 0.0
+    return call_count / length
+
+sms = csv_util('../../StudentLife_Dataset/Inputs/sms/')
 sms.readAll()
 sms.process(percentage)
 sms.writeToCsv(['uid', 'percentage'], 'sms.csv')
